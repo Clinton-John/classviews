9586ea6f316ebdd262886af2f3793a9486345926
clintonjohn-cmd <johnouko@kabarak.ac.ke>
2024-04-15 19:56:20 +0300

    template and redirect views

--- base/urls.py
+++ base/urls.py
@@ -1,12 +1,23 @@
 from django.urls import path, include
 from . import views
+from django.views.generic import TemplateView, RedirectView
+from base.views import ChatApp, AllTeams, SingleTeam
 
-urlpatterns = [
-   path('', views.home )
-]
+# urlpatterns = [
+#    path('', views.home )
+# ]s
 
 # using the CBVs
 # 1. Templateviews 
 urlpatterns =[
-    path('',)
+    path('', TemplateView.as_view(template_name="base/home.html", extra_context={'title':'Custom Title'})),
+    path('chat/', ChatApp.as_view(), name='chat'),
+    #redirectView
+    path('rdt', RedirectView.as_view(url='https://github.com/Clinton-John'), name='redirect_user'),
+    ## redrict users from all the pages to a single post page
+    path('viewteams/<str:pk>', AllTeams.as_view(), name='redirect_all_teams'),
+    path('viewSingleTeam/<str:pk>', SingleTeam.as_view(), name='redirect_single_team'),
+
+
+     
 ]
--- base/views.py
+++ base/views.py
@@ -1,6 +1,57 @@
 from django.shortcuts import render
-
+from django.views.generic.base import TemplateView, RedirectView
+from base.models import Post
+from django.shortcuts import get_object_or_404
+from django.db.models import F
 # Create your views here.
 
 def home(request):
     return render(request, 'base/home.html')
+
+class ChatApp(TemplateView):
+    #the below are the attributes that you can pass into this class to overwrite those in the initial class
+    template_name = 'base/chat.html'
+    # template_engine = default is DTL but you can change to allow for other features such as jinja2 and Mako
+    # response_class
+    # content_type
+
+    ## to add or return some context, you can overwrite the get_context_data function from the TemplateResponseMixin and retrieve data that you want to pass to the user/frontend
+
+    def get_context_data(self,  **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['posts'] = Post.objects.get(id=2)
+        context['data'] = "This is an example data passed"
+        #these function gets the context passed and passes them to the defined template above that is the chat.html
+        return context
+
+
+
+
+class AllTeams(RedirectView):
+    #url = ''
+    pattern_name = 'redirect_single_team' #this defines the pattern that you want to pass
+    #in this section, we override the get_redirect_url to set up the specific url we want
+    def get_redirect_url(self, *args, **kwargs):
+
+        # post = get_object_or_404(post, pk=kwargs['pk']) #find an instance of the model Post with the id pk, which is retrieved from the url
+        # post.count = F(count) + 1 #get the post with the id and then increment the count field by 1
+        # post.save()
+        ## this is an alternative way to redirect
+        post = Post.objects.filter(pk=kwargs['pk'])
+        post.update(count=F('count') + 1)
+        ## no need to use the post.save because we already used the update
+        return super().get_redirect_url(*args, **kwargs)
+       
+
+class SingleTeam(TemplateView):
+    template_name = 'base/post.html'
+
+    def get_context_data(self,  **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['posts'] = get_object_or_404(Post, pk= self.kwargs.get('pk'))
+        return context
+
+    # def get_context_data(self, **kwargs):
+    #     context = super().get_context_data(**kwargs)
+    #     context['posts'] = get_object_or_404(Post, pk= self.kwargs.get('pk'))
+    #     return context
